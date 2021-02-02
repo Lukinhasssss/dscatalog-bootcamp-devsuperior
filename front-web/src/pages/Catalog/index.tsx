6b8413a56da321c11e769c9e04d03c932e4f8bd0
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-02 09:03:41 -0300

    Add product listing with backend

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,25 +1,51 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
 import ProductCard from './components/ProductCard'
+import { makeRequest } from '../../core/utils/request'
+import { ProductsResponse } from '../../core/types/Product'
 
 import './styles.scss'
 
-const Catalog = () => (
-  <div className="catalog-container">
-    <h1 className="catalog-title">
-      Catálogo de produtos
-    </h1>
-    <div className="catalog-products">
-      <Link to="/products/1"><ProductCard /></Link>
-      <Link to="/products/2"><ProductCard /></Link>
-      <Link to="/products/3"><ProductCard /></Link>
-      <Link to="/products/4"><ProductCard /></Link>
-      <Link to="/products/5"><ProductCard /></Link>
-      <Link to="/products/6"><ProductCard /></Link>
-      <Link to="/products/7"><ProductCard /></Link>
-      <Link to="/products/8"><ProductCard /></Link>
-      <Link to="/products/9"><ProductCard /></Link>
+const Catalog = () => {
+  // Populando um estado no componente e listando os produtos dinâmicamente (quando a lista de produtos estiver disponível)
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>()
+
+  // Buscando a lista de produtos na inicialização do componente
+  useEffect(() => {
+    const params = {
+      page: 0,
+      linesPerPage: 12
+    }
+
+    makeRequest({ url: '/products', params })
+      .then(response => setProductsResponse(response.data))
+  }) // O primeiro parâmetro é uma function e o segundo parâmetro é uma lista de dependências. Quando a lista está vazia significa que a função vai disparar assim que o componente iniciar
+
+  return (
+    <div className="catalog-container">
+      <h1 className="catalog-title">
+        Catálogo de produtos
+      </h1>
+      <div className="catalog-products">
+        {productsResponse?.content.map(product => ( // Esse .content.map só vai acontecer quando o valor do productResponse não for undefined
+          <Link to={`/products/${product.id}`} key={ product.id }>
+            <ProductCard product={ product } />
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Catalog
+
+/*
+    Obs:
+
+        Desvantagens do fetch:
+
+          • Muito verboso
+          • Não tem suporte nativo para ler o progresso de upload de arquivos
+          • Não tem suporte nativo para enviar query strings
+*/
