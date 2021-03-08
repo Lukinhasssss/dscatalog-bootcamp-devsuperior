09cd6828621c56c211394c42e8a7a3d92e67f7bd
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-08 11:22:57 -0300

    Login form

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,3 +1,6 @@
+import ButtonIcon from 'core/components/ButtonIcon'
+import { Link } from 'react-router-dom'
+
 import AuthCard from '../Card'
 
 import './styles.scss'
@@ -6,7 +9,32 @@ const Login = () => {
   return (
     <AuthCard title="Login">
       <form className="login-form">
-        <h1>Formulário de Login</h1>
+        <input
+          type="email"
+          className="form-control input-base margin-bottom-30"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          className="form-control input-base"
+          placeholder="Senha"
+        />
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha?
+        </Link>
+
+        <div className="login-submit">
+          <ButtonIcon text="Logar" />
+        </div>
+
+        <div className="text-center"> {/* text-center é uma classe do Bootstrap */}
+          <span className="not-registered">
+            Não tem cadastro?
+          </span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
       </form>
     </AuthCard>
   )
