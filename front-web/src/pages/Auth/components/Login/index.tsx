38befd72d3f7a538be81231a2face5e3979cb394
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-08 12:42:40 -0300

    Add React Hook Form to manage forms

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,23 +1,40 @@
-import ButtonIcon from 'core/components/ButtonIcon'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
 
 import AuthCard from '../Card'
+import ButtonIcon from 'core/components/ButtonIcon'
 
 import './styles.scss'
 
+// Definindo o modelo de dados do formulário
+type FormData = {
+  email: string
+  password: string
+}
+
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>()
+
+  const onSubmit = (data: FormData) => {
+    console.log(data)
+  }
+
   return (
     <AuthCard title="Login">
-      <form className="login-form">
+      <form className="login-form" onSubmit={ handleSubmit(onSubmit) }>
         <input
           type="email"
           className="form-control input-base margin-bottom-30"
           placeholder="Email"
+          name="email"
+          ref={ register }
         />
         <input
           type="password"
           className="form-control input-base"
           placeholder="Senha"
+          name="senha"
+          ref={ register }
         />
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha?
