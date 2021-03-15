a4e629f698dfba4efeee579c52394c89d88811eb
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-15 11:21:29 -0300

    Add product registration validation

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -14,11 +14,11 @@ type FormState = {
 }
 
 const Form = () => {
-  const { register, handleSubmit } = useForm<FormState>()
+  const { register, handleSubmit, errors } = useForm<FormState>()
 
   const onSubmit = (data: FormState) => {
-    // console.log(data)
-    makePrivateRequest({ url:'/products', method: 'POST', data })
+    console.log(data)
+    // makePrivateRequest({ url:'/products', method: 'POST', data })
   }
 
   return (
@@ -26,27 +26,52 @@ const Form = () => {
       <BaseForm title="Cadastrar um produto">
         <div className="row">
           <div className="col-6">
-            <input
-              name="name" // É o atributo que identifica o campo
-              type="text"
-              className="form-control margin-bottom-30 input-base"
-              placeholder="Nome do produto"
-              ref={ register({ required: "Campo obrigatório" }) }
-            />
-            <input
-              name="price" // É o atributo que identifica o campo
-              type="number"
-              className="form-control margin-bottom-30 input-base"
-              placeholder="Preço"
-              ref={ register({ required: "Campo obrigatório" }) }
-            />
-            <input
-              name="imageUrl" // É o atributo que identifica o campo
-              type="text"
-              className="form-control margin-bottom-30 input-base"
-              placeholder="Imagem"
-              ref={ register({ required: "Campo obrigatório" }) }
-            />
+            <div className="margin-bottom-30">
+              <input
+                name="name" // É o atributo que identifica o campo
+                type="text"
+                className="form-control input-base"
+                placeholder="Nome do produto"
+                ref={register({
+                  required: "Campo obrigatório",
+                  minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres' },
+                  maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres' }
+                })}
+              />
+              {errors.name && (
+                <div className="invalid-feedback d-block">
+                  { errors.name.message }
+                </div>
+              )}
+            </div>
+            <div className="margin-bottom-30">
+              <input
+                name="price" // É o atributo que identifica o campo
+                type="number"
+                className="form-control input-base"
+                placeholder="Preço"
+                ref={ register({ required: "Campo obrigatório" }) }
+              />
+              {errors.price && (
+                <div className="invalid-feedback d-block">
+                  { errors.price.message }
+                </div>
+              )}
+            </div>
+            <div className="margin-bottom-30">
+              <input
+                name="imageUrl" // É o atributo que identifica o campo
+                type="text"
+                className="form-control input-base"
+                placeholder="Imagem"
+                ref={ register({ required: "Campo obrigatório" }) }
+              />
+              {errors.imageUrl && (
+                <div className="invalid-feedback d-block">
+                  { errors.imageUrl.message }
+                </div>
+              )}
+            </div>
           </div>
           <div className="col-6">
             <textarea
@@ -57,6 +82,11 @@ const Form = () => {
               cols={30}
               rows={10}
             />
+            {errors.description && (
+                <div className="invalid-feedback d-block">
+                  { errors.description.message }
+                </div>
+              )}
           </div>
         </div>
       </BaseForm>
