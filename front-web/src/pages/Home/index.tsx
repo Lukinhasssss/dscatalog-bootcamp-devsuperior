9bf71b566d10e83fff6cd16d2c641593e5fcacb2
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-20 12:07:25 -0300

    Home component in middle screen

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -7,13 +7,13 @@ import './styles.scss'
 
 const Home = () => (
   <div className="home-container">
-    <div className="row home-content card-base border-radius-20">
-      <div className="col-6">
+    <div className="home-content card-base border-radius-20">
+      <div className="home-text">
         <h1 className="text-title">Conheça o melhor <br/> catálogo de produtos</h1>
         <p className="text-subtitle">
           Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.
         </p>
-        <Link to="/products">
+        <Link to="/products" className="start-search-button">
           <ButtonIcon text="Inicie agora a sua busca" />
         </Link>
       </div>
